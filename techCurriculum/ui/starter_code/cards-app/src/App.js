de081f2cda4c722877154b15e0a46c5e3b2dd6ae
tadas412 <tadas412@gmail.com>
2017-05-13 16:09:44 -0400

    4.1 diff

--- techCurriculum/ui/starter_code/cards-app/src/App.js
+++ techCurriculum/ui/starter_code/cards-app/src/App.js
@@ -18,14 +18,37 @@ import React from 'react';
 import Title from './components/Title';
 import Card from './components/Card';
 
-function App() {
-  return (
-    <div>
-      <Title />
-      <Card author='John Smith' text='React is so cool!'/>
-      <Card author='Jane Doe' text='I use React for all my projects!'/>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cards: [
+        {
+          author: 'John Smith',
+          text: 'React is so cool!'
+        },
+        {
+          author: 'Jane Doe',
+          text: 'I use React for all my projects!'
+        }
+      ]
+    };
+  }
+  
+  render() {
+    const cards = this.state.cards.map((card, index) => (
+      <Card author={card.author}
+        text={card.text}
+        key={index} />
+    ));
+
+    return (
+      <div>
+        <Title />
+        { cards }
+      </div>
+    );
+  }
 }
 
 export default App;
